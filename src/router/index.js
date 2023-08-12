8b0539df54cec464a340e0227466e8052f73288b
sauloguerreiro <s.guerreiro@swordhealth.com>
2023-08-12 19:42:42 +0100

    feat: create experience card component

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
-import { createWebHistory, createRouter} from 'vue-router';
-import Home from '@/views/Home.vue';
+import {createRouter, createWebHistory, useRoute} from 'vue-router'
+import Home from '@/views/Home.vue'
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-    },
-    {
-        path: '/destination/:id/:slug',
-        name: 'destination.show',
-        component: () => import('@/views/DestinationShow.vue'),
-        props: route => ({
-            id: parseInt(route.params.id)
-        })
-    },
+  {path: '/', name: 'Home', component: Home},
+  {
+    path: '/destination/:id/:slug', 
+    name: 'destination.show', 
+    component: ()=>import('@/views/DestinationShow.vue'),
+    props: route=> ({...route.params, id: parseInt(route.params.id)}),
+    children: [
+        {
+          path: ':experienceSlug',
+          name: 'experience.show',
+          component: () => import('@/views/ExperienceShow.vue'),
+          props: route=> ({...route.params, id: parseInt(route.params.id)})
+        }
+    ],
+  },
 ]
 
-
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
-
-export default router;
+  history: createWebHistory(),
+  routes
+})
+export default router
