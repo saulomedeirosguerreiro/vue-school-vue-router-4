41c1190fdf64bf79a71a122d71bcaa23e17bf666
sauloguerreiro <s.guerreiro@swordhealth.com>
2023-08-12 18:10:29 +0100

    feat: create destination show component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,5 @@
 import { createWebHistory, createRouter} from 'vue-router';
-import Home from '@/components/Home.vue';
-import About from '@/components/About.vue';
-
-import Brazil from '@/views/Brazil.vue';
-import Jamaica from '@/views/Jamaica.vue';
-import Hawaii from '@/views/Hawaii.vue';
-import Panama from '@/views/Panama.vue';
+import Home from '@/views/Home.vue';
 
 const routes = [
     {
@@ -14,29 +8,9 @@ const routes = [
         component: Home,
     },
     {
-        path: '/about',
-        name: 'About',
-        component: About,
-    },
-    {
-        path: '/brazil',
-        name: 'Brazil',
-        component: Brazil,
-    },
-    {
-        path: '/jamaica',
-        name: 'Jamaica',
-        component: Jamaica,
-    },
-    {
-        path: '/hawaii',
-        name: 'Hawaii',
-        component: Hawaii,
-    },
-    {
-        path: '/panama',
-        name: 'Panama',
-        component: Panama,
+        path: '/destination/:id',
+        name: 'destination.show',
+        component: () => import('@/views/DestinationShow.vue'),
     },
 ]
 
