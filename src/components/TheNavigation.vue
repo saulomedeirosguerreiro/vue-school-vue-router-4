9d7bbde4bd7dab93ac508c1c7147d59f6fb7254f
sauloguerreiro <s.guerreiro@swordhealth.com>
2023-08-13 16:24:18 +0100

    feat: add not found page, go back and app-link

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,22 +1,24 @@
 <template>
-    <div id="nav">
-      <router-link id="logo" to="/">Vue School Travel App</router-link>
-      <router-link
-          v-for="destination in destinations"
-          :key="destination.id"
-          :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}}"
-        >
-          {{ destination.name }}
-        </router-link>
-    </div>
-  </template>
-  <script>
-  import sourceData from '@/data.json'
-  export default {
-    data(){
-      return {
-        destinations: sourceData.destinations
-      }
+  <div id="nav">
+    <AppLink id="logo" to="/">Vue School Travel App</AppLink>
+    <AppLink
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}}"
+      >
+        {{ destination.name }}
+      </AppLink>
+      <AppLink :to="{name: 'protected'}">Dashboard</AppLink>
+      <AppLink to="https://vueschool.io">Vue School</AppLink>
+  </div>
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+  data(){
+    return {
+      destinations: sourceData.destinations
     }
   }
-  </script>
+}
+</script>
